067ca87e9d8212eacee722423ed2f3bcabfabcf8
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-04-30 19:42:09 +0200

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />
+      }
+    ]
+  }
+])
 
 export default function App() {
   return (
-    <Homepage />
+    <RouterProvider router={router} />
+  )
+}
+
+function Layout() {
+  return(
+    <div>
+
+      <Outlet />
+    </div>
   )
 }
